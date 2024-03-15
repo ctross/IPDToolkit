648cfdc6695e5366b0f7bcecb9907886f3bbe151
ctross <ctross@ucdavis.edu>
2024-03-15 21:38:51 +0100

    Update setup_folders.R

--- R/setup_folders.R
+++ R/setup_folders.R
@@ -4,7 +4,7 @@
 #' @param 
 #' path Full path to where folder will be stored. This folder will be called "PrisonersDilema".
 #' @param 
-#' import_code If TRUE, this will import R and Stan template code from the PrisonR R package, and save it in the new directory. 
+#' import_code If TRUE, this will import R and Stan template code from the IPDToolkit R package, and save it in the new directory. 
 #' @param
 #' overwrite Setting this to TRUE will OVERWRITE existing files!
 #' @export
@@ -20,35 +20,35 @@ setup_folders = function(path=path, import_code=TRUE, overwrite=FALSE){
   }
 
    if(import_code==TRUE){
-  file.copy(paste0(path.package("PrisonR"),"/","WACKO.R"), paste0(path,"/","PrisonersDilema","/","StrategiesR/","WACKO.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","GLUM.R"), paste0(path,"/","PrisonersDilema","/","StrategiesR/","GLUM.R"), overwrite=overwrite)
 
-  file.copy(paste0(path.package("PrisonR"),"/","truth_table_atft.csv"), paste0(path,"/","PrisonersDilema","/","DataTables/","truth_table_atft.csv"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","truth_table_atft.csv"), paste0(path,"/","PrisonersDilema","/","DataTables/","truth_table_atft.csv"), overwrite=overwrite)
 
-  file.copy(paste0(path.package("PrisonR"),"/","Functions.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Functions.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Functions.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Functions.R"), overwrite=overwrite)
   
-  file.copy(paste0(path.package("PrisonR"),"/","Data.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Data.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","Parameters.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Parameters.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","Model.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Model.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Data.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Data.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Parameters.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Parameters.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Model.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Model.R"), overwrite=overwrite)
 
-  file.copy(paste0(path.package("PrisonR"),"/","Data_CovariateModel.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Data_CovariateModel.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","Parameters_CovariateModel.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Parameters_CovariateModel.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","Model_CovariateModel.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Model_CovariateModel.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Data_CovariateModel.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Data_CovariateModel.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Parameters_CovariateModel.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Parameters_CovariateModel.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","Model_CovariateModel.R"), paste0(path,"/","PrisonersDilema","/","StanCode/","Model_CovariateModel.R"), overwrite=overwrite)
 
-  file.copy(paste0(path.package("PrisonR"),"/","ALLC.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","ALLC.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","ALLD.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","ALLD.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","RANDY.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","RANDY.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyTFT.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTFT.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyGTFT.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyGTFT.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyTF2T.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTF2T.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyWSLS.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyWSLS.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyTFTA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTFTA.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyGTFTA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyGTFTA.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyTF2TA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTF2TA.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyWSLSA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyWSLSA.R"), overwrite=overwrite)
-  file.copy(paste0(path.package("PrisonR"),"/","SneakyATFT.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyATFT.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","ALLC.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","ALLC.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","ALLD.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","ALLD.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","RANDY.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","RANDY.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyTFT.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTFT.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyGTFT.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyGTFT.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyTF2T.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTF2T.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyWSLS.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyWSLS.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyTFTA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTFTA.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyGTFTA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyGTFTA.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyTF2TA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyTF2TA.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyWSLSA.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyWSLSA.R"), overwrite=overwrite)
+  file.copy(paste0(path.package("IPDToolkit"),"/","SneakyATFT.R"), paste0(path,"/","PrisonersDilema","/","StrategiesStan/","SneakyATFT.R"), overwrite=overwrite)
   
 
-  tt <- read.csv(paste0(path.package("PrisonR"),"/","truth_table_atft.csv"))
+  tt <- read.csv(paste0(path.package("IPDToolkit"),"/","truth_table_atft.csv"))
   tt$error_called <- ifelse((tt$partners_observed_move != tt$partners_move_arb_declared) & tt$partners_observed_move == 0, 1, 0)
   tt$error_called[is.na(tt$error_called)] <- 0
   tt$move_choice <- ifelse(tt$new_focal_standing==0 | tt$new_partner_standing==1,1,0) 
